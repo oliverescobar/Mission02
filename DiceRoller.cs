2c4e3157744b7507626f0c8742929fc6f66af340
oliverescobar <oliverescobar@OLIVERESCOB37E5>
2024-01-17 23:41:33 -0700

    Add project files.

---
+++ DiceRoller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Mission02
+{
+    internal class DiceRoller
+    {
+        private Random r = new Random();
+        public int[] RollDice(int rolls)
+        {
+            int[] results = new int[11];
+
+            for (int i = 0; i < rolls; i++)
+            {
+                int dice1 = r.Next(1, 7);
+                int dice2 = r.Next(1, 7);
+
+                int sum = dice1 + dice2;
+                results[sum - 2]++;
+            }
+
+            return results;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Mission02;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        System.Console.WriteLine("Welcome to the dice throwing simulator!"); /* necessary code to display*/
+        System.Console.WriteLine("How many dice rolls would you like to simulate?");
+        int rolls; /* defining integer variable */
+        rolls = int.Parse(System.Console.ReadLine());
+        DiceRoller dr = new DiceRoller(); /* creating an instance (?) */
+        int[] results = dr.RollDice(rolls); /* calling the array */
+        System.Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+        System.Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        System.Console.WriteLine("Total number of rolls = " + rolls);
+
+        for (int i = 0; i < results.Length; i++) /* loop to help display the code */
+        {
+            double percentage = (double)results[i] / rolls * 100;
+            System.Console.Write($"{i + 2}: {new string('*', (int)Math.Round(percentage))} "); /* apparently, write and write line are different ... */
+            System.Console.WriteLine($"({percentage:F2}%)");
+        }
+
+        System.Console.WriteLine("\nThank you for using the Dice Rolling Simulator. Goodbye!");
+    }
+}
